7931d4bae582d93b13cb92053849fc4cbeba3880
Acrafto <xoelsd@gmail.com>
2025-04-05 21:33:15 +0200

    No big changes here

--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 from linked_ordered_positional_list import LinkedOrderedPositionalList as ListaOrdenada
 #from array_ordered_positional_list import ArrayOrderedPositionalList as ListaOrdenada
+from Car_Classes import *
+def procces_order(model_name,concesionario): #This will later be used to process the order
+    """Process an order for a specific model."""
+    return None
 
-def read_orders(path="pedidos.txt"):
+def read_orders_and_procces(path, concesionario): #This will read the orders from a file and process them
     with open(path) as f:
         for l in f.readlines():
             ls = l.strip().split(",")
             customer, model_name = ls[0], ls[1]
-            print(f"Por hacer: procesar pedido {model_name} del cliente {customer}")
+            print(f"Nuevo pedido: {model_name}--{customer}")
+            procces_order(model_name,concesionario)
 
-def read_parts(path="piezas.txt"):
-    with open(path) as f:
-        for l in f.readlines():
-            ls = l.strip().split(",")
-            part_name, qty = ls[0], int(ls[1])
-            print(f"Por hacer: añadir al inventario la pieza \"{part_name}\" con ({qty} unidades)")   
 
-def read_models(path="modelos.txt"):
-    with open(path) as f:
-        for l in f.readlines():
-            ls = l.strip().split(",")
-            model_name, part_name, qty = ls[0], ls[1], int(ls[2])
-            print(f"Por hacer: añadir al catálogo pieza \"{part_name}\" ({qty} unidades) al modelo \"{model_name}\"")
 			
 if __name__ == "__main__":
-    read_parts()
-    read_models() 
-    read_orders()
-            
+    concesionario=Concesionario()
+    inventario=concesionario.inventario
+    catalogo=concesionario.catalogo
+    inventario.read_parts_and_add_to_inventory()
+    catalogo.read_models_and_add_to_catalogue()
+    print("--------STOCK--------")
+    print(inventario) 
+    print("--------CATALOGO-------- ")
+    print(catalogo)
+    read_orders_and_procces("pedidos.txt", concesionario)
