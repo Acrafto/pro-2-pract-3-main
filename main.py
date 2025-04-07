5c7a0a16475860d4b95e7cc0ca6a037340d957ea
Acrafto <xoelsd@gmail.com>
2025-04-07 18:27:16 +0200

    En principio esta hecho, faltan Docstrings

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from linked_ordered_positional_list import LinkedOrderedPositionalList as ListaOrdenada
 #from array_ordered_positional_list import ArrayOrderedPositionalList as ListaOrdenada
 from Car_Classes import *
-def procces_order(model_name,concesionario): #This will later be used to process the order
+
+def procces_order(orders:list,concesionario:Concesionario): #This will later be used to process the order
     """Process an order for a specific model."""
-    return None
+    for model_name,customer in orders:
+        print(f"Nuevo pedido: {model_name}--{customer}")
+        concesionario.procces_order(model_name) #This will process the order
+        print("--------STOCK--------")
+        print(inventario) 
+        print("---------------------")
+    return True
 
 def read_orders_and_procces(path, concesionario): #This will read the orders from a file and process them
     with open(path) as f:
+        orders=[]
         for l in f.readlines():
             ls = l.strip().split(",")
             customer, model_name = ls[0], ls[1]
-            print(f"Nuevo pedido: {model_name}--{customer}")
-            procces_order(model_name,concesionario)
+            orders.append((model_name,customer))
+            #print(f"Por hacer: procesar pedido de modelo {model_name} para cliente {customer}") # Old print from original code
+    return procces_order(orders, concesionario) #This will process the order
 
 
 			
@@ -25,4 +34,7 @@ if __name__ == "__main__":
     print(inventario) 
     print("--------CATALOGO-------- ")
     print(catalogo)
-    read_orders_and_procces("pedidos.txt", concesionario)
+    finished=read_orders_and_procces("pedidos.txt", concesionario)
+    if finished:
+        print("--------CATALOGO-------- ")
+        print(catalogo)
